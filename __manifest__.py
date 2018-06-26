5173bb3052d5c1baf0e37e79d5230f1c0b2bc95c
kdesai <odoo_dev@venusinformatics.com>
2018-06-26 18:32:25 +1200

    security groups added

--- __manifest__.py
+++ __manifest__.py
@@ -2,5 +2,9 @@
     'name': "Library Books", 
     'summary': "Manage your books", 
     'depends': ['base'], 
-    'data': ['views/library_book.xml'], 
+    'data': [
+        'security/groups.xml',
+        'views/library_book.xml',
+        'security/ir.model.access.csv',
+        ],
 }
