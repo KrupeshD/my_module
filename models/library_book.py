23b8e472db2a3703bb79908bf939e4b52b24f9e6
venusinfo <githubadmin@venusinformatics.com>
2018-07-03 18:23:13 +1200

    Added new fields in the model

--- models/library_book.py
+++ models/library_book.py
@@ -1,9 +1,60 @@
 from odoo import models, fields
 class LibraryBook(models.Model):
+
+    # structural attributes defining the behavior #
     _name = 'library.book'
+    _description = 'Library Book'
+    _order = 'date_release desc, name'
+    _rec_name = 'short_name'
+
+    # Fields #
+
     name = fields.Char('Title', required=True)
-    date_release = fields.Date('Release Date')
-    author_ids = fields.Many2many(
-        'res.partner',
-        string='Authors'
+
+    short_name = fields.Char(
+        string='Short Title',
+        size=100,  # For Char only
+        translate=False,  # also for Text fields
     )
+
+    date_release = fields.Date('Release Date')
+    author_ids = fields.Many2many('res.partner',string='Authors')
+
+    notes = fields.Text('Internal Notes')
+    state = fields.Selection(
+        [('draft', 'Not Available'),
+         ('available', 'Available'),
+         ('lost', 'Lost')],
+        'State')
+
+    description = fields.Html(
+        string='Description',
+        # optional:
+        sanitize=True,
+        strip_style=False,
+        translate=False,
+    )
+
+    cover = fields.Binary('Book Cover')
+    out_of_print = fields.Boolean('Out of Print?')
+    date_release = fields.Date('Release Date')
+    date_updated = fields.Datetime('Last Updated')
+
+    pages = fields.Integer(
+        string='Number of Pages',
+        default=0,
+        help='Total book page count',
+        groups='base.group_user',
+        states={'lost': [('readonly', True)]},
+        copy=True,
+        index=False,
+        readonly=False,
+        required=False,
+        company_dependent=False,
+    )
+
+    reader_rating = fields.Float(
+        'Reader Average Rating',
+        digits=(14, 4),  # Optional precision (total, decimals),
+    )
+
