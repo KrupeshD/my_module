c7a23e21120f6289b54857e0f3dca78a02e24192
venusinfo <githubadmin@venusinformatics.com>
2018-07-28 10:57:08 +1200

    modified the loan wizard

--- models/library_book_loan.py
+++ models/library_book_loan.py
@@ -17,13 +17,16 @@ class LibraryLoanWizard(models.TransientModel):
     member_id = fields.Many2one('library.member', string='Member')
     book_ids = fields.Many2many('library.book', string='Books')
 
+
     @api.multi
     def record_loans(self):
-        loan = self.env['library.book.loan']
         for wizard in self:
-            member = wizard.member_id
             books = wizard.book_ids
+            loan = self.env['library.book.loan']
             for book in books:
-                loan.create({'member_id': member.id,
-                             'book_id': book.id})
-
+                values = wizard._prepare_loan(book)
+                loan.create(values)
+     @api.multi
+     def _prepare_loan(self, book):
+         return {'member_id': self.member_id.id,
+                 'book_id': book.id}
